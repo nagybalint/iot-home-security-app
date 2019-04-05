9d23b78e28969a67dab9bc325e99060c8448ce03
nagybalint <nagybalint000@gmail.com>
2019-04-05 21:31:08 +0200

    Add styling for AuthScreen

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
+
+import Banner from '../components/Banner';
 
 class AuthScreen extends Component {
     render() {
         return(
-        <View>
-            <Text>Hello AuthScreen</Text>
-        </View>
+            <View style={styles.containerStyle}>
+                <View style={styles.bannerContainerStyle}>
+                    <Banner />
+                </View>
+                <View style={styles.buttonContainerStyle}>
+                    <Button 
+                        title="Register"
+                        buttonStyle={styles.buttonStyle}
+                    />
+                    <Button 
+                        title="Log In"
+                        buttonStyle={styles.buttonStyle}
+                    />
+                </View>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        padding: 6,
+        flex: 1
+    },
+    bannerContainerStyle: {
+        marginTop: 60
+    },
+    buttonContainerStyle: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "stretch",
+        padding: 12
+    },
+    buttonStyle: {
+        margin: 3,
+        height:70,
+        backgroundColor: "#3D6DCC"
+    }
+});
 
 export default AuthScreen;
