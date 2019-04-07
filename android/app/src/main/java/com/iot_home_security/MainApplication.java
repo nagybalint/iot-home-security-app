b63e0c5ca7426d3e81c410aae3debcb8bfbf5825
nagybalint <nagybalint000@gmail.com>
2019-04-07 10:52:11 +0200

    Add firebase auth to the project

--- android/app/src/main/java/com/iot_home_security/MainApplication.java
+++ android/app/src/main/java/com/iot_home_security/MainApplication.java
@@ -10,6 +10,7 @@ import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -30,13 +31,14 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNGestureHandlerPackage(),
-            new AsyncStoragePackage(),
+          new VectorIconsPackage(),
+          new RNGestureHandlerPackage(),
+          new AsyncStoragePackage(),
           new RNFirebasePackage(),
           new RNFirebaseMessagingPackage(),
           new RNFirebaseNotificationsPackage(),
-          new RNFirebaseFirestorePackage()
+          new RNFirebaseFirestorePackage(),
+          new RNFirebaseAuthPackage()
       );
     }
 
